e9f922d86c4119675f53959ab78f9e461f338fbc
jenkins <jenkins@git>
2021-07-16 18:20:24 +0430

    api

--- src/api/todo.js
+++ src/api/todo.js
@@ -1,3 +1,7 @@
 import axios from './axios';
 
 export const getTodoList = () => axios.get('/todo');
+export const getTodo = id => axios.get(`/todo/${id}`);
+export const postTodo = data => axios.post('/todo/', data);
+export const updateToDo = (id, data) => axios.put(`/todo/${id}`, data);
+export const deleteToDo = id => axios.delete(`/todo/${id}`);
